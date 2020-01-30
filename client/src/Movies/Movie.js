b8c0362ca6896d69e4d7ef27bf3df7a4fcbd5ce7
[Arin Ramer] <[arinramer@gmail.com]>
2020-01-29 16:34:36 -0800

    Done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import axios from "axios";
 import MovieCard from "./MovieCard";
 export default class Movie extends React.Component {
@@ -42,6 +43,7 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <button><Link to={`/update-movie/${this.props.match.params.id}`} style={{textDecoration: "none", color: "black"}}>Edit Movie</Link></button>
       </div>
     );
   }
